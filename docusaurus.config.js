00c5d971dfb15c94c52078e65cb14f94efd9b7c2
Patricia McPhee <hi@patriciamcphee.com>
2022-02-05 19:15:19 -0800

    update config base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Patricia McPhee',
   tagline: 'UX & Tech Writing Samples',
-  url: 'https://patriciamcphee.github.io/portfolio/',
+  url: 'https://patriciamcphee.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

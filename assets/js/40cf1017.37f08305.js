4f44557b722b05f41b0ec12afc9d5c172fa9998d
Patricia McPhee <hi@patriciamcphee.com>
2022-02-25 12:54:16 -0800

    Deploy website - based on 259b73da6ae74be8fc2b4b62cab4f4c12ecff587

---
+++ assets/js/40cf1017.37f08305.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1990],{1359:function(e){e.exports=JSON.parse('{"permalink":"/portfolio/blog/tags/technical-writing","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);

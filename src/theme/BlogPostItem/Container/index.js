93f1e98dba29f79c8024db0d70d905af46822824
Patricia McPhee <hi@patriciamcphee.com>
2024-12-20 09:13:36 -0500

    remove messagerWidget

--- src/theme/BlogPostItem/Container/index.js
+++ src/theme/BlogPostItem/Container/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 import {useBlogPost} from '@docusaurus/theme-common/internal';
+
 export default function BlogPostItemContainer({children, className}) {
   const {
     frontMatter,
@@ -11,25 +12,7 @@ export default function BlogPostItemContainer({children, className}) {
   const image = assets.image ?? frontMatter.image;
   const keywords = frontMatter.keywords ?? [];
 
-
-  window.erxesSettings = {
-    messenger: {
-      brand_id: "_-LCq3",
-    },
-  };
-  
-  (() => {
-    const script = document.createElement('script');
-    script.src = "https://pscmdesigns.app.erxes.io/widgets/build/messengerWidget.bundle.js";
-    script.async = true;
-
-    const entry = document.getElementsByTagName('script')[0];
-    entry.parentNode.insertBefore(script, entry);
-  })();
-
-
   return (
-    
     <article
       className={className}
       itemProp="blogPost"

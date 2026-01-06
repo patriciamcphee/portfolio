b288f118ba107ad633bcbea78fe6bf2babd828a9
Patricia McPhee <hi@patriciamcphee.com>
2026-01-06 15:10:28 -0500

    remove extra config file

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+    watch: {
+      exclude: ['samples/**']
+    }
+  },
+  server: {
+    watch: {
+      ignored: ['**/samples/**']
+    }
+  },
 });
